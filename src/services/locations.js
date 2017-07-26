fad7be86a01f8c62db88e4ed70497af1bbd0d73b
Kevin Mircovich <kmircovich1@gmail.com>
2017-07-26 14:44:29 -0400

    updated info

--- src/services/locations.js
+++ src/services/locations.js
@@ -8,41 +8,47 @@ angular.module('sampleApp')
             name: 'Tampa Convention Center',
             address: '333 S Franklin St, Tampa, FL 33602',
             description: 'This is where we\'ll be departing from on July 31. BE ON TIME!',
-            imageName: 'tampa-convention-center'
+            imageName: 'tampa-convention-center',
+            icon: 'location_city'
           },
           {
-            name: 'Hotel in Columbia, SC',
-            address: '',
-            description: 'Hotel we\'ll  be staying at in Columbia, SC',
-            imageName: 'tampa-convention-center'
+            name: 'Hotel in Savanah, GA',
+            address: 'address tbd',
+            description: 'Hotel we\'ll  be staying at in GA',
+            imageName: 'tampa-convention-center',
+            icon: 'local_hotel'
           },
           {
             name: 'Advent Coworking',
             website: 'https://adventcoworking.com/',
             address: '933 Louise Ave #101, Charlotte, NC 28204',
             description: 'Advent Coworking will host us for breakfast/lunch and offer their space to work at.',
-            imageName: 'advent'
+            imageName: '',
+            icon: 'pin_drop'
           },
           {
             name: 'Catawba Brewing Company',
             website: 'catawbabrewing.com',
             address: '212 S Green St, Morganton, NC 28655',
             description: 'Always be Pitching!',
-            imageName: 'catawba'
+            imageName: 'catawba',
+            icon: 'local_bar'
           },
           {
             name: 'Nashville Downtown Hostel',
             website: 'https://www.nashvilledowntownhostel.com',
             address: '177 1st Ave N, Nashville, TN 37201',
             description: 'We\'ll spend night number two at this amazing hostel.',
-            imageName: 'catawba'
+            imageName: 'catawba',
+            icon: 'local_hotel'
           },
           {
             name: 'Clarion Hotel Grand Boutique',
             website: 'https://www.choicehotels.com/louisiana/new-orleans/clarion-hotels/la087',
             address: '2001 St Charles Ave New Orleans, LA 70130',
             description: 'All of StartupBus North America at one location!',
-            imageName: 'clarion'
+            imageName: 'clarion',
+            icon: 'local_hotel'
           },
         ]);
       });
--- src/services/people.js
+++ src/services/people.js
@@ -6,9 +6,9 @@ angular.module('sampleApp')
         resolve([
           { firstName: 'Kevin', lastName: 'Mircovich', title: 'Conductor', phone: '(727) 678-5785', email: 'kmircovich1@gmail.com', color: '#3DDB93', city: 'St. Petersburg, FL' },
           { firstName: 'Brent', lastName: 'Henderson', title: 'Conductor', phone: '(727) 434-2130', email: 'bhenderson3690@gmail.com ', color: '#E44444', city: 'Orlando, FL' },
-          { firstName: 'Ray', lastName: 'Land', title: 'Litteral Conductor', color: '#EF8D2E', city: 'Gainsville, FL' },
+          { firstName: 'Ray', lastName: 'Land', title: 'Literal Conductor', color: '#EF8D2E', city: 'Gainsville, FL' },
         ]);
       });
-    },
+    }
   };
 });
