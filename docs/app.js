0224d865a9c2fa23d6b3c817ad6c9fbc9c72df3f
Kevin Mircovich <kmircovich1@gmail.com>
2017-07-28 14:41:04 -0400

    build docs

--- docs/app.js
+++ docs/app.js
@@ -80518,7 +80518,7 @@ angular.module('sampleApp')
         resolve([
           { firstName: 'Kevin', lastName: 'Mircovich', title: 'Conductor', phone: '(727) 678-5785', email: 'kmircovich1@gmail.com', color: '#3DDB93', city: 'St. Petersburg, FL' },
           { firstName: 'Brent', lastName: 'Henderson', title: 'Conductor', phone: '(727) 434-2130', email: 'bhenderson3690@gmail.com ', color: '#E44444', city: 'Orlando, FL' },
-          { firstName: 'Ray', lastName: 'Land', title: 'Literal Conductor', color: '#EF8D2E', city: 'Gainsville, FL' },
+          { firstName: 'Ray', lastName: 'Land', title: 'Driver', color: '#EF8D2E', city: 'Gainsville, FL' },
           { firstName: 'Justin', lastName: 'Davis', title: 'Mentor (Day 3)', color: '#89e2ff', city: 'Tampa, FL' },
         ]);
       });
